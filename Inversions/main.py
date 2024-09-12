9246838cd5c68845289320fe2255343d0dffe6b6
Fischer Wells <fisch.wells@gmail.com>
2024-09-11 18:12:11 -0600

    created file for inversion project

---
+++ Inversions/main.py
@@ -0,0 +1,16 @@
+def mergesort(array):
+    '''run mergesort on a given list'''
+    if type(array) != list:
+        print("Please enter a list")
+    else:
+        print("mergesorting")
+
+
+def main():
+    a = "word"
+    b = [1,2,3,4,5]
+    mergesort(a)
+    mergesort(b)
+
+
+main()
